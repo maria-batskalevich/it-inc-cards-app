7034394d9cfd1408371dc144de3a367f3d71dc46
maria-batskalevich <maria.batskalevich@gmail.com>
2022-12-08 18:47:53 +0300

    test process.env

--- src/01-API/auth-api.ts
+++ src/01-API/auth-api.ts
@@ -7,9 +7,10 @@ import {
 } from "./ApiResponseTypes";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
+    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
     // baseURL: 'http://localhost:7542/2.0/',
     // baseURL: 'https://neko-back.herokuapp.com/2.0',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
