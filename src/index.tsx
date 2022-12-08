512f34fcc9d964c40b7b354d5ced361a00b7030f
maria-batskalevich <maria.batskalevich@gmail.com>
2022-12-08 18:47:43 +0300

    test process.env

--- src/index.tsx
+++ src/index.tsx
@@ -16,8 +16,6 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+console.log(process.env)
+console.log(process.env.REACT_APP_BACK_URL)
 reportWebVitals();
